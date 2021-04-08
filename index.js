b465dce2df36cf9de0092e1f13f740518df39b5d
simonemayers <simone21295@gmail.com>
2021-04-08 08:07:07 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+// Divide function expression
+const divide = function(){
+    return 2000/100
+}
+//Square arrow function 
+const square = n => n*n; 
+
+//Add arrow function 
+const add = (a, b) => a+b; 
